b07b909aae256c23c970d89c8dba4a8223d77def
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-11-19 11:59:08 +0200

    initial commit - spring boot application

---
+++ src/main/java/org/db/connectors/UltimateDatabaseConnectorApplication.java
@@ -0,0 +1,15 @@
+package org.db.connectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
+@EnableSwagger2
+@SpringBootApplication
+public class UltimateDatabaseConnectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateDatabaseConnectorApplication.class, args);
+    }
+}
