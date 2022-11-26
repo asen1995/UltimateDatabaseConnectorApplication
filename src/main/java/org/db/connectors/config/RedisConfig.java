acbf146afd13e359aa2651dde72df091aa4e9ad6
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-11-26 15:48:21 +0200

    create redis connection pool for performance

---
+++ src/main/java/org/db/connectors/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package org.db.connectors.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
+@Configuration
+@ConditionalOnExpression(
+        "${redis.enabled:true}"
+)
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+    @Value("${redis.port}")
+    private int redisPort;
+
+
+    @Bean
+    public JedisPool redisConnectionPool() {
+
+        final JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(128);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+        poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+        poolConfig.setNumTestsPerEvictionRun(3);
+        poolConfig.setBlockWhenExhausted(true);
+
+        return new JedisPool(poolConfig, redisHost, redisPort);
+    }
+}
--- src/main/java/org/db/connectors/repository/ProductRedisRepositoryImpl.java
+++ src/main/java/org/db/connectors/repository/ProductRedisRepositoryImpl.java
@@ -4,21 +4,23 @@ import org.db.connectors.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @Repository
 public class ProductRedisRepositoryImpl implements ProductRedisRepository {
 
     @Autowired(required = false)
-    Jedis jedis;
+    JedisPool jedisPool;
 
     @Override
     public String insertRedisProduct(Product product) {
-        return jedis.set(String.valueOf(product.getProductId()), product.getProductName());
+        return jedisPool.getResource().set(String.valueOf(product.getProductId()), product.getProductName());
     }
 
     @Override
     public Product getProductById(String productId) {
-        final String productName = jedis.get(productId);
+        final Jedis jedisConnection = jedisPool.getResource();
+        final String productName = jedisConnection.get(productId);
         if (productName == null || productName.isEmpty()) {
             throw new RuntimeException(String.format("Can't find product with productId %s ", productId));
         }
